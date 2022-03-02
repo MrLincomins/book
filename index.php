c75fd146b892fd242fc006b6175fd38f85ff0f09
Damir SH <100434690+MrLincomins@users.noreply.github.com>
2022-03-02 07:55:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <li><a href="search.php">Поиск книг по автору</a></li>
     <li><a href="top100.php">Топ 100 авторов</a></li>
     <li><a href="YearBook.php">Поиск книг по году</a></li>
+    <li><a href="dinamic.php">Среднее количество книг автором за год</a></li>
+    <li><a href="add.php">Добавление книг</a></li>
   </ul>
 </nav>
 <title>Главная</title>
