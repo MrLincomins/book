e0c7a0f39541269401c90d8fada99b76e60e6916
Damir SH <100434690+MrLincomins@users.noreply.github.com>
2022-03-17 20:12:03 +0300

    Update top100.php

--- top100.php
+++ top100.php
@@ -2,9 +2,9 @@
 session_start();
 require_once 'bd.php';
 $sql = "SELECT Author, count(*) books_count FROM books GROUP BY Author ORDER BY books_count DESC LIMIT 100";
-if ($result = mysqli_query($db, $sql)) {
+if ($result = $dbO->query($sql)) {
   echo "<table><tr><th>Авторы</th><th>Количество книг</th></tr>";
-  foreach($result as $row){
+  foreach ($result as $row){
     echo "<tr>";
      echo "<td>" . $row["Author"] . "</td>";
      echo "<td>" . $row["books_count"] . "</td>";
